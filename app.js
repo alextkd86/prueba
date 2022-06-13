247c74fc6254734c98ce614209fd04efea5a4c32
Alejandro Pascual <apascual@externos.grupopsn.es>
2022-06-13 14:58:38 +0200

    okkdem

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.json({'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'})
+});
+
+app.post('/loginFake', (req, res) => {
+	const pepe = req.params.res;
+	res.status(200).json({'isValid': true, 'token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
